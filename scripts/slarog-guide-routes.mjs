3395468424c8999c24880fba48f233c44a34dc5a
nkis-works <nkis.base@gmail.com>
2026-09-17 00:13:52 +0900

    Define Slarog guide routes

---
+++ scripts/slarog-guide-routes.mjs
@@ -0,0 +1,8 @@
+export function slarogGuideRoutes() {
+  return [
+    '/products/slarog/guides/',
+    '/products/slarog/guides/save-slump-graph/',
+    '/products/slarog/guides/connect-multiple-days/',
+    '/products/slarog/guides/align-zero-and-scale/',
+  ];
+}
